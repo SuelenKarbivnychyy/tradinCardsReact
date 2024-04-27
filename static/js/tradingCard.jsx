cb38c639498fc05261c321f1be17890368968c0a
Suelen Karbivnychyy <suelenmatosr@gmail.com>
2024-04-27 14:15:12 -0400

    Updated TradingCardContainer to make usage of useState to clean up the code and get the data from server.

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -1,53 +1,3 @@
-const tradingCardData = [
-  {
-    name: 'Balloonicorn',
-    skill: 'video games',
-    imgUrl: '/static/img/balloonicorn.jpg',
-    cardId: 1,
-  },
-  {
-    name: 'Float',
-    skill: 'baking pretzels',
-    imgUrl: '/static/img/float.jpg',
-    cardId: 2,
-  },
-  {
-    name: 'Llambda',
-    skill: 'knitting scarves',
-    imgUrl: '/static/img/llambda.jpg',
-    cardId: 3,
-  },
-  {
-    name: 'Off-By-One',
-    skill: 'climbing mountains',
-    imgUrl: '/static/img/off-by-one.jpeg',
-    cardId: 4,
-  },
-  {
-    name: 'Seed.py',
-    skill: 'making curry dishes',
-    imgUrl: '/static/img/seedpy.jpeg',
-    cardId: 5,
-  },
-  {
-    name: 'Polymorphism',
-    skill: 'costumes',
-    imgUrl: '/static/img/polymorphism.jpeg',
-    cardId: 6,
-  },
-  {
-    name: 'Short Stack Overflow',
-    skill: 'ocean animal trivia',
-    imgUrl: '/static/img/shortstack-overflow.jpeg',
-    cardId: 7,
-  },
-  {
-    name: 'Merge',
-    skill: 'bullet journaling',
-    imgUrl: '/static/img/merge.png',
-    cardId: 8,
-  },
-];
 
 function TradingCard(props) {
   return (
@@ -59,10 +9,20 @@ function TradingCard(props) {
   );
 }
 
-function TradingCardContainer() {
+
+
+function TradingCardContainer() {  
+
+  const floatCard = {
+    name: 'Float',
+    skill: 'baking pretzels',
+    imgUrl: '/static/img/float.jpg'
+  };
+
+  const [cards, setCard] = React.useState([floatCard])
   const tradingCards = [];
 
-  for (const currentCard of tradingCardData) {
+  for (const currentCard of cards) {
     tradingCards.push(
       <TradingCard
         key={currentCard.cardId}
@@ -73,7 +33,11 @@ function TradingCardContainer() {
     );
   }
 
-  return <div className="grid">{tradingCards}</div>;
+  return (<div className="grid">{tradingCards}</div>);
+
+
 }
+
+
 
 ReactDOM.render(<TradingCardContainer />, document.getElementById('container'));
