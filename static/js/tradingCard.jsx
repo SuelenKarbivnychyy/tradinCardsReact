b8f8f007f28c239bf71b099212a91db8a3bbe12d
Suelen Karbivnychyy <suelenmatosr@gmail.com>
2024-04-27 15:46:21 -0400

    Developed functionality to add new card to page

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -10,17 +10,22 @@ function TradingCard(props) {
 }
 
 
+function TradingCardContainer() {
+  const [cards, setCards] = React.useState([]) 
 
-function TradingCardContainer() {  
+  function addCard(newCard) {
+    const currentCards = [...cards];
 
-  const floatCard = {
-    name: 'Float',
-    skill: 'baking pretzels',
-    imgUrl: '/static/img/float.jpg'
-  };
+    setCards([...currentCards, newCard]);
+  }
 
-  const [cards, setCard] = React.useState([floatCard])
   const tradingCards = [];
+  React.useEffect(() => {
+    fetch('/cards.json')
+    .then((response) => response.json())
+    .then((result) => setCards(result.cards))
+    }, []);  
+
 
   for (const currentCard of cards) {
     tradingCards.push(
@@ -33,11 +38,73 @@ function TradingCardContainer() {
     );
   }
 
-  return (<div className="grid">{tradingCards}</div>);
-
+  return (
+    <React.Fragment>
+      <AddTradingCard addCard={addCard} />
+      <h2>Trading Cards</h2>
+      <div className="grid">{tradingCards}</div>
+    </React.Fragment>
+  );
 
 }
 
-
-
 ReactDOM.render(<TradingCardContainer />, document.getElementById('container'));
+
+
+
+function AddTradingCard(props) {
+  
+  const [name, setName] = React.useState("");
+  const [skill, setSkill] = React.useState("");
+  
+
+  const imputData = {
+    'name': name,
+    'skill': skill
+  };
+
+
+  function addNewCard() {
+
+    fetch('/add-card', {
+      method:'POST',
+      body: JSON.stringify(imputData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((answer) => {
+         const cardAdded = answer.cardAdded;
+         props.addCard(cardAdded);
+      });
+      
+  }
+
+  return (
+    <React.Fragment>
+      <h2>Add New Trading Card</h2>
+      <label htmlFor="nameInput">Name</label>
+      <input
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+        id="nameInput"
+        style={{ marginLeft: "5px" }}
+      ></input>
+      <label
+        htmlFor="skillInput"
+        style={{ marginLeft: "10px", marginRight: "5px" }}
+      >
+        Skill
+      </label>
+      <input
+        value={skill}
+        onChange={(event) => setSkill(event.target.value)}
+        id="skillInput"
+      ></input>
+      <button style={{ marginLeft: "10px" }} onClick={addNewCard}>
+        Add
+      </button>
+    </React.Fragment>
+  );
+}
